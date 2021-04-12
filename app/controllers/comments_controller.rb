a619309cc8118b254ba1f6f0c02c69c79c02f3e7
Renan Ribeiro <renan@itexto.com.br>
2021-04-11 22:17:42 -0300

    finalizando o tutorial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,22 @@
 class CommentsController < ApplicationController
 
+    http_basic_authenticate_with name: "ddh", password: "secret", only: :destroy
+
     def create
       @article = Article.find(params[:article_id])
       @comment = @article.comments.create(comment_params)
       redirect_to article_path(@article)
     end
 
+    def destroy
+      @article = Article.find(params[article_id])
+      @comment = @article.comment.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+
     private
       def comment_params
-        params.require(:comment).permit(:commenter, :body)
+        params.require(:comment).permit(:commenter, :body, :status)
       end
 
 end
